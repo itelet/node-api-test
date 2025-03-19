6e24f3a5e515f89b16e5488b43de24c7b1a4c58f
itelet <szabo.adam.0725@gmail.com>
2025-03-19 14:08:23 +0100

    Add api/{routes} endpoint

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
+app.use("/api/", routes);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
