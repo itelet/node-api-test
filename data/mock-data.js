95cda3a770f5d9007cf55ba5432b1e795d1f5dac
itelet <szabo.adam.0725@gmail.com>
2025-03-19 14:33:49 +0100

    Add comments to mock data

--- data/mock-data.js
+++ data/mock-data.js
@@ -1,4 +1,4 @@
-module.exports = [
+const mockPostResponse = [
 	{
 		id: 1,
 		title: "...",
@@ -6,6 +6,20 @@ module.exports = [
 		body: "...",
 		created_at: "2023-02-11",
 		tags: ["Sports"],
+		comments: [
+			{
+				"id": 1,
+				"created_at": "2023-02-13",
+				"author": "Test User A",
+				"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+			},
+			{
+				"id": 2,
+				"created_at": "2023-02-12",
+				"author": "Test User B",
+				"body": "Etiam tincidunt fermentum felis, quis luctus lectus suscipit nec."
+			}
+		]
 	},
 	{
 		id: 2,
@@ -14,6 +28,14 @@ module.exports = [
 		body: "...",
 		created_at: "2023-02-10",
 		tags: ["Business", "Tech"],
+		comments: [
+			{
+				"id": 1,
+				"created_at": "2023-02-13",
+				"author": "Test User A",
+				"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+			}
+		]
 	},
 	{
 		id: 3,
@@ -22,5 +44,21 @@ module.exports = [
 		body: "...",
 		created_at: "2023-02-09",
 		tags: ["Economy"],
+		comments: [
+			{
+				"id": 1,
+				"created_at": "2023-02-13",
+				"author": "Test User A",
+				"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+			},
+			{
+				"id": 2,
+				"created_at": "2023-02-12",
+				"author": "Test User B",
+				"body": "Etiam tincidunt fermentum felis, quis luctus lectus suscipit nec."
+			}
+		]
 	},
 ];
+
+module.exports = { mockPostResponse };
